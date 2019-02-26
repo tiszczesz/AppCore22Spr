8d2ba340820405cd8a4abc37d25910dbb7c90053
pszczesz <pszczesz@poczta.onet.pl>
2019-02-26 11:25:14 +0100

    areas1

---
+++ AppCore22Spr/Areas/Nowa/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCore22Spr.Areas.Nowa.Controllers
+{
+    [Area("Nowa")]
+    [Route("/nowa")]
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppCore22Spr/Areas/Stara/Controllers/DefaultController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppCore22Spr.Areas.Stara.Controllers
+{
+    [Area("Stara")]
+    [Route("/stara")]
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
